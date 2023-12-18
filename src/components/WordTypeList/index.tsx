804f165a1cd765395924c4d2d1162edff4dd5692
黄超 <huangchao.hello@bytedance.com>
2023-12-18 13:58:49 +0800

    新增WordTypeHelper实例类

--- src/components/WordTypeList/index.tsx
+++ src/components/WordTypeList/index.tsx
@@ -1,20 +1,18 @@
-import {useState} from "react";
-import isEqual from 'lodash/isEqual';
 import {WordTypeItem} from "./WordTypeItem.tsx";
-import {formatWordTypeInfo, WordTypeInfo} from "../../models";
+import {useWordTypeHelper, WordTypeHelper, WordTypeInfo} from "../../models";
 
-const testValue: WordTypeInfo[] = [
+const helper = new WordTypeHelper([
   { typeKey: '01', name: '测试-01', color: 'yellow', backGroundColor: '', description: '这是一段测试' },
   { typeKey: '02', name: '测试-02', color: 'green', backGroundColor: '', description: '这是一段测试' },
   { typeKey: '03', name: '测试-03', color: 'yellow', backGroundColor: '', description: '这是一段测试' },
   { typeKey: '04', name: '测试-04', color: 'green', backGroundColor: '', description: '这是一段测试' },
-]
+]);
 
 export function WordTypeList() {
-  const [list, setList] = useState<WordTypeInfo[]>(testValue);
+  const [typeHelper] = useWordTypeHelper(helper);
   return (
     <div>
-      {list.map((v: WordTypeInfo, index: number) => (
+      {typeHelper.list.map((v: WordTypeInfo, index: number) => (
         <WordTypeItem
           key={v.typeKey}
           typeKey={v.typeKey}
@@ -24,15 +22,7 @@ export function WordTypeList() {
           description={v.description}
           selected={v.typeKey === '04'}
           onTypeInfoChange={info => {
-            const formatV = formatWordTypeInfo(info);
-            setList(oldV => {
-              if (isEqual(oldV[index], formatV)) {
-                return oldV;
-              }
-              const newV = [...oldV];
-              newV[index] = formatV;
-              return newV;
-            });
+            typeHelper.update(index, info);
           }}
         />
       ))}
--- src/models/wordType.ts
+++ src/models/wordType.ts
@@ -1,3 +1,5 @@
+import {useEffect, useMemo, useState} from "react";
+
 export interface WordTypeInfo {
   // 类型的key(UUID)
   typeKey: string;
@@ -19,4 +21,141 @@ export function formatWordTypeInfo(info: Partial<WordTypeInfo> | Record<string, 
     backGroundColor: info.backGroundColor || '',
     description: info.description || '',
   }
+}
+
+export class WordTypeHelper {
+  // word类型数组
+  protected array: WordTypeInfo[];
+  // array数组的版本号
+  protected version: number;
+  // 缓存空间
+  protected cacheRecord: Record<string, any> = {};
+  // 数组变更监听函数
+  protected eventListeners = new Set<(arr: WordTypeInfo[]) => void>();
+
+  get list(): WordTypeInfo[] {
+    if (this.version !== this.cacheRecord.list_version) {
+      this.cacheRecord.list_array = [...this.array];
+      this.cacheRecord.list_version = this.version;
+    }
+    return this.cacheRecord.list_array;
+  }
+
+  constructor(arr: WordTypeInfo[]) {
+    this.array = arr;
+    this.version = 0;
+  }
+
+  /**
+   * 向前移动
+   * @param index 要操作的元素index
+   */
+  toFront(index: number) {
+    if (index <= 0) {
+      return false;
+    } else if (index >= this.array.length) {
+      return false;
+    }
+    const left = this.array[index - 1];
+    this.array[index - 1] = this.array[index];
+    this.array[index] = left;
+    this.version++;
+    this.dispatch();
+    return true;
+  }
+
+  /**
+   * 向后移动
+   * @param index 要操作的元素index
+   */
+  toEnd(index: number) {
+    if (index < 0) {
+      return false;
+    } else if (index >= this.array.length - 1) {
+      return false;
+    }
+    const right = this.array[index + 1];
+    this.array[index + 1] = this.array[index];
+    this.array[index] = right;
+    this.version++;
+    this.dispatch();
+    return true;
+  }
+
+  /**
+   * 追加元素
+   * @param item  要追加的元素
+   */
+  push(item: WordTypeInfo) {
+    const v = this.array.push(item);
+    this.version++;
+    this.dispatch();
+    return v;
+  }
+
+  /**
+   * 更新元素数据
+   * @param index 要更新的元素
+   * @param item  要更新的内容
+   */
+  update(index: number, item: Partial<WordTypeInfo>) {
+    if (index < 0) {
+      return false;
+    } else if (index >= this.array.length) {
+      return false;
+    }
+    this.array[index] = formatWordTypeInfo({
+      ...this.array[index],
+      ...item,
+    });
+    this.version++;
+    this.dispatch();
+    return true;
+  }
+
+  /**
+   * 删除元素
+   * @param index 要追加的元素
+   */
+  delete(index: number) {
+    if (index < 0) {
+      return false;
+    } else if (index >= this.array.length) {
+      return false;
+    }
+    this.array.splice(index, 1);
+    this.version++;
+    this.dispatch();
+    return true;
+  }
+
+  addListener(cb: (arr: WordTypeInfo[]) => void) {
+    return this.eventListeners.add(cb);
+  }
+
+  removeListener(cb: (arr: WordTypeInfo[]) => void) {
+    return this.eventListeners.delete(cb);
+  }
+
+  dispatch() {
+    for (const listener of this.eventListeners) {
+      listener(this.list);
+    }
+  }
+}
+
+export function useWordTypeHelper(data: WordTypeHelper | WordTypeInfo[]) {
+  const helper = useMemo(() => {
+    if (data instanceof WordTypeHelper) {
+      return data;
+    }
+    return new WordTypeHelper(data);
+  }, [data]);
+  const [refresh, setRefresh] = useState(Number.MIN_SAFE_INTEGER);
+  useEffect(() => {
+    const cb = () => setRefresh(v => v + 1);
+    helper.addListener(cb);
+    return () => void helper.removeListener(cb);
+  }, [helper]);
+  return [helper, refresh, setRefresh] as const;
 }
